fb46e864dbf66e9ba6bdc2517a98e326f7f509dc
Aprotim Biswas <aprotim1983@example.com>
2023-02-05 23:46:46 +0530

    Fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,19 +87,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': os.getenv("DATABASE_NAME"),  
-        'USER': os.getenv("DATABASE_USERNAME"),  
-        'PASSWORD': os.getenv("DATABASE_PASSWORD"),  
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'TEST': {
-            'NAME': f'test_{os.getenv("DATABASE_NAME")}',
-        } 
-    }  
-}  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
