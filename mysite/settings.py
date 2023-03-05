197716cf2e474d8217cee7b60559284bdab439a0
Aprotim Biswas <biswasaprotim2011@outlook.com>
2023-03-05 20:11:31 +0530

    Again

--- mysite/settings.py
+++ mysite/settings.py
@@ -194,7 +194,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("172.17.0.2/16", 6379)],
+            "hosts": [("172.17.0.1", 6379)],
         },
     },
 }
