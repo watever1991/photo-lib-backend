ef5d9d90dd22a37f6ea16f40a8d13896eb6b101b
Aprotim Biswas <biswasaprotim2011@outlook.com>
2023-03-05 17:50:55 +0530

    Host Fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -194,7 +194,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("myhost", 6379)],
         },
     },
 }
